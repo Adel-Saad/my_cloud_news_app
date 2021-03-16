c65affbe88184e325cbd5ca217718a43fc9afd7b
Adel Saad <adelalderby@gmail.com>
2021-03-16 15:57:11 +0300

    my_news_app

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cloud News App'),
+      ),
+      body: Center(
+        child: Text('Hello Alino'),
+      ),
+    );
+  }
+}
